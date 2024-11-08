b90f51499acbeec9f808213d2d55959c585bab2b
ahmed733 <ahmedayaansyed06@gmail.com>
2024-11-08 17:04:43 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -6,14 +6,23 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
+import path from "path";
 
 dotenv.config();
+
+const __dirname = path.resolve();
 
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 // app.get("/", (req, res) => {
 //   res.send("Hello World!");
